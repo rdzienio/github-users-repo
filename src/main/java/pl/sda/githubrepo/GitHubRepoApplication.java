6cc329fb76b35a37b3b69ac097295e30a3cfd42b
Robert Dzienio <robertdzienio@gmail.com>
2020-07-14 09:52:06 +0200

    git init

---
+++ src/main/java/pl/sda/githubrepo/GitHubRepoApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.githubrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/githubrepo/GitHubRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.githubrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
