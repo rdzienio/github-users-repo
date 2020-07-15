69c5932f1bdcb28883e8451a3ba497fbaaf6b4c6
Robert Dzienio <robertdzienio@gmail.com>
2020-07-15 11:45:24 +0200

    [change] pretty html page

--- src/main/java/pl/sda/controller/GithubWebController.java
+++ src/main/java/pl/sda/controller/GithubWebController.java
@@ -1,20 +1,29 @@
 package pl.sda.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.sda.service.GithubService;
 
 @Controller
 public class GithubWebController {
 
-    private final GithubService service;
+    private static final String ALL_GITHUB_REPOS = "githubRepos";
+    private static final String USER_NAME = "userName";
 
-    public GithubWebController(final GithubService service) {
+    private final GithubService service;
+    private final String githubUser;
+
+    public GithubWebController(final GithubService service, @Value("${github.user}") final String githubUser) {
         this.service = service;
+        this.githubUser = githubUser;
     }
 
-    @GetMapping(path = "/*")
-    public String getGithubRepositories(){
+    @GetMapping(path = {"index","/*"})
+    public String getGithubRepositories(Model model){
+        model.addAttribute(USER_NAME, githubUser);
+        model.addAttribute(ALL_GITHUB_REPOS, service.getGithubRepositories());
         return "index";
     }
 }
